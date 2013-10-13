8e5427b462e7b410670e55014cb1f4d8ada16d6a
Alexander Nielsen <alex@hitecherik.tk>
2013-10-13 18:23:03 +0100

    Small email functionality bug fixes.

--- js/read.js
+++ js/read.js
@@ -23,16 +23,25 @@
 		});
 	});
 	
-	$("#mailSend").on("click", function(){
+	$(".mailSend").on("click", function(){
 		vex.dialog.open({
 			message: "Share by email",
 			input: '<input type="email" name="emailTo" placeholder="Send this email to..." required="required"><input type="email" name="emailFrom" placeholder="Send this email from..." required="required"><input type="text" name="nameFrom" placeholder="My name is..." required="required"><input type="checkbox" name="cc" value="true"> Send a copy to me too!',
 			callback: function(data){
-				var cc = (data.cc ? "true" : "false");
-				
-				$.get("send_email.php?emailTo=" + encodeURIComponent(data.emailTo) + "&emailFrom=" + encodeURIComponent(data.emailFrom) + "&nameFrom=" + encodeURIComponent(data.nameFrom) + "&url=" + encodeURIComponent(article.url) + "&cc=" + cc + "&excerpt=" + encodeURIComponent(article.excerpt) + "&title=" + encodeURIComponent(article.title), function(){
-					vex.dialog.alert("Email successfully sent!");
-				});
+				if(data!==false){
+					var cc = (data.cc ? "true" : "false");
+					
+					$.get("send_email.php?emailTo=" + encodeURIComponent(data.emailTo) + "&emailFrom=" + encodeURIComponent(data.emailFrom) + "&nameFrom=" + encodeURIComponent(data.nameFrom) + "&url=" + encodeURIComponent(article.url) + "&cc=" + cc + "&excerpt=" + encodeURIComponent(article.excerpt) + "&title=" + encodeURIComponent(article.title), function(){
+						vex.dialog.alert("Email successfully sent!");
+					});
+				}
+			},
+			afterOpen: function(){
+				if(IE){
+					$(".vex-content input").blur();
+				} else {
+					$(".vex-content input").first().focus();
+				}
 			}
 		});
 	});
--- read.php
+++ read.php
@@ -69,48 +69,9 @@
 			<a href="http://readability.com/save?url=<?php echo $rddme; ?>" class="pure-button readability-button pure-button-custom" role="button" target="_blank">Save to Readability</a>
 			<a href="http://twitter.com/intent/tweet?url=<?php echo urlencode($isgd); ?>&text=<?php echo urlencode($title); ?>&related=hitecherik%3AThis+Just+In+creator" target="_blank" class="pure-button pure-button-custom tweet-button" role="button">Tweet</a>
 			<a href="http://www.facebook.com/sharer/sharer.php?u=<?php echo $isgd; ?>" target="_blank" class="pure-button facebook-button pure-button-custom" role="button">Share on Facebook</a>
-			<!--<a href="#mailSend" class="pure-button" data-toggle="modal" role="button">Send by Email</a>-->
-			<button class="pure-button" id="mailSend">Send by Email</button>
+			<button class="pure-button mailSend">Send by Email</button>
 		</p>
 	</div>
-	
-	<!--<div class="modal fade" id="mailSend" data-backdrop="static">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h2 class="modal-title">Share by Email</h2>
-				</div>
-				<div class="modal-body">
-					<p class="warning-text">We do not store anything that is entered into this form. We only use the data entered to send your email.</p>
-					<form action="#" class="pure-form pure-form-aligned">
-						<fieldset>
-							<div class="pure-control-group">
-								<label for="emailTo">Send this email <b>to</b>:</label>
-								<input type="email" name="emailTo">
-							</div>
-							<div class="pure-control-group">
-								<label for="emailFrom">Send it <b>from</b> this email:</label>
-								<input type="email" name="emailFrom">
-							</div>
-							<div class="pure-control-group">
-								<label for="nameFrom">My name is:</label>
-								<input type="text" name="nameFrom">
-							</div>
-							<div class="pure-controls">
-								<label for="cc">
-									<input type="checkbox" name="cc" value="true"> Send a copy to me too!
-								</label>
-								<p><button type="submit" class="pure-button pure-button-primary">Send Email</button></p>
-							</div>
-						</fieldset>
-					</form>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="pure-button" title="Cancel" data-dismiss="modal">Close</button>
-				</div>
-			</div>
-		</div>
-	</div>-->
 	
 	<script src="js/read.js"></script>
 	<script src="js/placeholders.min.js"></script>
