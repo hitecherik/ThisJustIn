f86aac462782009e360042925c68356abf4bec6d
Alexander Nielsen <hitecherik@hitecherik.site11.com>
2014-02-02 10:02:09 +0000

    Changed copyright date to 2014.

--- sidebar.php
+++ sidebar.php
@@ -33,5 +33,5 @@
 
 <div class="about-section">
 	<p>Read the four latest stories from any of our 10 news sections, or add your own custom RSS feed!</p>
-	<p>Copyright &copy; <a href="http://hitecherik.net" target="_blank">Alexander Nielsen</a>, 2013, under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> license.</p>
+	<p>Copyright &copy; <a href="http://hitecherik.net" target="_blank">Alexander Nielsen</a>, 2013 - <?php echo date("Y"); ?>, under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> license.</p>
 </div>
